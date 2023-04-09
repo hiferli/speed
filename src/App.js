9f19fb9348916b7c1562d5527f36e809f8dee487
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-09 19:29:27 +0530

    Alternative Method (BETA)

--- src/App.js
+++ src/App.js
@@ -2,68 +2,33 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+	const [speed, setSpeed] = useState(0);
+	const [locationGiven, setLocationGiven] = useState(false);
 
-	const [latitude, setLatitude] = useState(undefined);
-	const [longitude, setLongitude] = useState(undefined);
-	const [distance, setDistance] = useState(undefined);
-
-	const [isLocationGranted, setIsLocationGranted] = useState(false);
-
-	const locationGranted = (position) => {
-		setIsLocationGranted(true)
-		setLatitude(position.coords.latitude);
-		setLongitude(position.coords.longitude);
-	};
-
-	const locationDenied = () => {
-		console.log("Location Service Denied");
-	};
-
-	function calculateDistance(lat1, lon1, lat2, lon2) {
-		var p = 0.017453292519943295;    // Math.PI / 180
-		var c = Math.cos;
-		var a = 0.5 - c((lat2 - lat1) * p) / 2 +
-			c(lat1 * p) * c(lat2 * p) *
-			(1 - c((lon2 - lon1) * p)) / 2;
-
-		return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+	function haveLocation(position) {
+		setLocationGiven(true);
+		setSpeed(position.coords.speed);
+		console.log(position.coords.speed);
 	}
 
+	function dontHaveLocation() {
+		console.log("No Location Detected");
+	}
 
 	useEffect(() => {
-		setInterval(() => {
-			if (latitude === undefined && longitude === undefined) {
-				navigator.geolocation.getCurrentPosition((position) => locationGranted(position), locationDenied);
-			} else {
-				let oldLatitude = latitude;
-				let oldLongitude = longitude;
-				navigator.geolocation.getCurrentPosition((position) => locationGranted(position), locationDenied);
-				
-				let distance = calculateDistance(oldLatitude, oldLongitude, latitude, longitude);
-				setDistance(distance);
-				console.log(distance)
-			}
-		}, 1000);
-	}, [latitude , longitude]);
+		// setInterval(() => {
+			navigator.geolocation.watchPosition((position) => haveLocation(position) , dontHaveLocation);
+		// }, 1000);
+	}, []);
 
 	return (
 		<div className="App">
 			{
-				isLocationGranted ? 
+				locationGiven ?
 					<div className="locationGranted">
-						<h1>
-							Latitude: {latitude}
-						</h1>
-
-						<h1>
-							Longitude: {longitude}
-						</h1>
-
-						<h1>
-							Distance: {distance}
-						</h1>
+						<h1>Speed: {speed === null ? 0 : speed}</h1>
 					</div>
-				:
+					:
 					<div className="locationDenied">
 						<h1>Please enable location to get your speed</h1>
 					</div>
