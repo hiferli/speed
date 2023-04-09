1d92375349734f3f8774f7df88549ac4cc47fbc5
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-04-10 00:10:28 +0530

    Another Methodology to Implement

--- src/App.js
+++ src/App.js
@@ -1,49 +1,63 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-	const [speed, setSpeed] = useState(0);
-	const [locationGiven, setLocationGiven] = useState(false);
-
-	function haveLocation(position) {
-		setLocationGiven(true);
-		setSpeed(position.coords.speed !== (undefined || null) ? (position.coords.speed).toFixed(1) : 0);
-		console.log(position.coords.speed !== (undefined || null) ? (position.coords.speed).toFixed(1) : 0);
+	const [speed, setSpeed] = useState(undefined);
+	const [isLocationGranted, setIsLocationGranted] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
+	
+	function locationGranterd(position) {
+		setIsLocationGranted(true);
+		setSpeed(position.coords.speed);
+		console.log(position.coords.speed);
 	}
 
-	function dontHaveLocation() {
-		console.log("No Location Detected");
+	function locationDenied() {
+		setIsLocationGranted(false);
+		setErrorMessage("Please Enable Your Location Services!");
 	}
 
 	useEffect(() => {
-		setInterval(() => {
-			navigator.geolocation.watchPosition((position) => haveLocation(position) , dontHaveLocation);
+		const interval = setInterval(() => {
+
+			if(navigator.geolocation){
+				navigator.geolocation.watchPosition((position) => locationGranterd(position) , locationDenied);
+			} else {
+				setIsLocationGranted(false);
+				setErrorMessage("Geolocation is not supported on This Device. Try using some other device!");
+			}
+
 		}, 1000);
+		
+		return () => clearInterval(interval);
 	}, []);
 
-	return (
-		<div className="App">
+  return (
+    <div className="App">
 			{
-				locationGiven ?
-					<div className="locationGranted">
-						<h1>Speed: {speed === null ? "~0"  : speed} Meters/Second</h1>
+				isLocationGranted ? 
+					<div className="granted">
+						<h2>{speed === null ? 0 : speed} M/S</h2>
+						<h2>{speed * 3.6} KM/H</h2>
+						<h2>{speed * 2.237} Miles/H</h2>
 					</div>
+					
 					:
-					<div className="locationDenied">
-						<h1>Please enable location to get your speed</h1>
+
+					<div className="denied">
+						<h1>{errorMessage}</h1>
 					</div>
 			}
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
 
 /*
 Backbone Code:
-
 setInterval(() => {
-		navigator.geolocation.getCurrentPosition((position) => { console.log("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude) } , () => { console.log("Error getting location")})
+    navigator.geolocation.getCurrentPosition((position) => { console.log("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude) } , () => { console.log("Error getting location")})
 }, 1000);
 
 */
